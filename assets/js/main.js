2177f8b239ca5f04b0b217e07cefbaf2d15f312e
Sergey Hayevoy <jantao.dev@gmail.com>
2020-04-08 16:02:09 +0300

    Change recaptcha fetch to submit action

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,7 +80,14 @@ function activateSubmitBtn(e) {
   }
 }
 
-submitBtn.addEventListener('click', submitUserData);
+submitBtn.addEventListener('click', function () {
+  if (grecaptcha) {
+    grecaptcha.ready(submitUserData);
+  } else {
+    submitUserData
+  }
+});
+
 function submitUserData() {
   submitBtn.classList.add('btn-spinner');
   [].forEach.call(fields, (inp) => {
@@ -101,17 +108,19 @@ function submitUserData() {
   } else {
     submittedData.districtId = null;
   }
-  submittedData.recaptchaToken = window.recaptchaToken;
 
-  console.log(submittedData);
-  fetch('https://p40.laywagif.com/api/preorders', {
-    method: 'post',
-    headers: {
-      Accept: 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(submittedData),
-  })
+  grecaptcha.execute('6Lcs0-cUAAAAAIYIBZ8LbZg_uGGkYXnTZ1J3m5Kf', {action: 'homepage'})
+    .then((token) => {
+      submittedData.recaptchaToken = token;
+      return fetch('https://p40.laywagif.com/api/preorders', {
+        method: 'post',
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(submittedData),
+      });
+    })
     .then((res) => res.json())
     .then((res) => {
       submitBtn.classList.remove('btn-spinner');
